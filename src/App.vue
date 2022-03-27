74fa5e31f2e0f36b37d6053b8b51a85dda866f42
Thomas <thomasfindlay94@gmail.com>
2022-03-27 14:31:06 +0100

    work

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,67 @@
   <div :id="$style.app">
     <h1>Posts</h1>
 
-    <Grid :data-source="posts">
-      <GridColumn field="id" title="Post ID" />
-      <GridColumn field="userId" title="User ID" />
-      <GridColumn field="title" title="Title" />
-      <GridColumn field="excerpt" title="Excerpt" />
-    </Grid>
-    <!-- <table class="k-table">
-      <thead>
-        <tr>
-          <th>Post ID</th>
-          <th>User ID</th>
-          <th>Title</th>
-          <th>Description</th>
+    <table class="k-table k-table-layout-fixed">
+      <thead class="k-table-thead">
+        <tr class="k-table-row">
+          <th class="k-table-th">Post ID</th>
+          <th class="k-table-th">User ID</th>
+          <th class="k-table-th">Title</th>
+          <th class="k-table-th">Excerpt</th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="post of posts" :key="post.id">
-          <td>{{ post.id }}</td>
-          <td>{{ post.userId }}</td>
-          <td>{{ post.title }}</td>
-          <td>{{ post.description?.slice(0, 50) }}</td>
+      <tbody class="k-table-tbody">
+        <tr class="k-table-row" v-for="post of posts" :key="post.id">
+          <td class="k-table-td">{{ post.id }}</td>
+          <td class="k-table-td">{{ post.userId }}</td>
+          <td class="k-table-td">{{ post.title }}</td>
+          <td class="k-table-td">{{ post.excerpt?.slice(0, 50) }}</td>
         </tr>
       </tbody>
-    </table> -->
+    </table>
+    <div class="k-mt-8">
+      <Pager
+        :skip="skip"
+        :take="take"
+        :total="allPosts.length"
+        :buttonCount="5"
+        type="numeric"
+        info
+        pageSizes
+        previousNext
+        @pagechange="onPageChange"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import '@progress/kendo-ui/js/kendo.grid';
-import { Grid, GridColumn } from '@progress/kendo-grid-vue-wrapper';
+import { ref, computed } from "vue";
+import { Pager } from "@progress/kendo-vue-data-tools";
 
-const posts = ref([]);
+const skip = ref(0);
+const take = ref(10);
+const allPosts = ref([]);
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => {
-    console.log('test', json);
-    posts.value = json.map((item) => {
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(json => {
+    allPosts.value = json.map(item => {
       return {
         ...item,
         excerpt: item.body.slice(0, 50),
       };
     });
   });
+
+const posts = computed(() =>
+  allPosts.value.slice(skip.value, skip.value + take.value)
+);
+
+const onPageChange = event => {
+  skip.value = event.skip;
+  take.value = event.take;
+};
 </script>
 
 <style module>
